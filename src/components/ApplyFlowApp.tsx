83bda8ffcf8371f2c8983c780f833254d4e407ab
pgantaa <gantap@vcu.edu>
2025-08-24 21:59:39 -0400

    Home-8

--- src/components/ApplyFlowApp.tsx
+++ src/components/ApplyFlowApp.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useMemo, useState, ElementType, ReactNode } from "react";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
